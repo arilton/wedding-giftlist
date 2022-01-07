60bd775523321d3060075c85e70f9059d1108344
arilton <arilton.japf@gmail.com>
2022-01-07 09:43:31 -0300

    primeira versao da lista

---
+++ src/components/CardList/CardList.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Card, Row, Col} from "react-bootstrap";
+import Items from "./giftlist.json";
+
+const CardList = props => 
+    (<Row xs={1} sm={2} md={3} lg={4} xl={5} xxl={6} className="g-4">
+        {Items.map((item, idx) => (
+            <Col key={idx} sm={4}>
+                <Card style={{
+                    height: "100%",
+                    margin: "1rem",
+                    padding: "1rem"}}>
+                    <Card.Img
+                        style={{objectFit: "contain", height: "75%" }}
+                        variant="top"
+                        src={require(`./img/${item.image}`)}/>
+                    <Card.Body>
+                        <Card.Title>{item.title}</Card.Title>
+                        <Card.Text>
+                            <a href={item.link}>Ver modelo de referência</a>
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </Col>
+        ))}
+    </Row>);
+
+export default CardList;
