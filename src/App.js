c686bcb3434fd661f7b4685bb41599c636ea33e3
arilton <arilton.japf@gmail.com>
2022-01-07 09:52:10 -0300

    novos itens e atualizaçao do .lock

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import CardList from './components/CardList/CardList';
 function App() {
   return (
     <div className="App">
+      <h1 className='center'>PIX 84986315757</h1>
       <CardList />
     </div>
   );
